bee77f3f6e141a415f2b9cc3751a9818f1527951
Norton794 <nortonsantos79@gmail.com>
2023-08-15 09:02:06 -0300

    feat: Initial project setup and configuration

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}.`);
+});
